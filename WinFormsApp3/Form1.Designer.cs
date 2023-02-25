eb5541e8fe3926c4b413cb945ca7999b5e897bc4
Qscy <Qscy2011@sina.com>
2023-02-25 18:06:10 +0800

    添加项目文件。

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WinFormsApp3
+{
+    partial class InitWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.Location = new System.Drawing.Point(138, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "正在初始化...";
+            // 
+            // InitWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WinFormsApp3.Properties.Resources._20181228081531660;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(361, 154);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InitWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.InitWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,24 @@
+namespace WinFormsApp3
+{
+    public partial class InitWindow : Form
+    {
+        public InitWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitWindow_Load(object sender, EventArgs e)
+        {
+            var WecahtPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\WeChat Files\\";
+            var subPath = "\\FileStorage\\File\\";
+            List<string> userList = new();
+            List<string> monthList = new();
+            List<string> fileList = new();
+            var subFolders = Path.s;
+            //foreach (string folder in )
+            {
+
+            }
+        }
+    }
+}
